c474de913d7f66ab2aab84ff5a99a10a55f82123
zhuliang <zhuliang@huohua.cn>
2020-01-05 21:01:50 +0800

    登陆页面

--- src/main/java/com/zhuliang/oauth/config/WebBeanConfig.java
+++ src/main/java/com/zhuliang/oauth/config/WebBeanConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -30,7 +30,7 @@ public class WebBeanConfig {
    
    @Bean
    public PasswordEncoder passwordEncoder() {
-       return NoOpPasswordEncoder.getInstance();
+       return new BCryptPasswordEncoder(10);
    }
    
    /**
